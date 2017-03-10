414f9783831fa7c62f5d90da4532f21c563f99ef
ElijahButers <Elijahbuters@gmail.com>
2017-03-10 13:48:37 +0200

    Added code to the AppDelegate class in the application(didFinishLaunchingWithOptions) method that configures our application to use our API server for Stormpath.

--- Stormpath Notes/AppDelegate.swift
+++ Stormpath Notes/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Stormpath.sharedSession.configuration.APIURL = URL(string: "https://stormpathnotes.herokuapp.com")!
+        
         return true
     }
 
